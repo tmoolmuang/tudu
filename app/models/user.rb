32b23bc06c52417f50767f50e285de3d4ec0ed31
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-06-05 03:20:59 +0000

    add user

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+ 
+  before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :standard }
+
+  validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :email,
+             presence: true,
+             uniqueness: { case_sensitive: false },
+             length: { minimum: 3, maximum: 254 }
+         
+  enum role: [:standard, :admin]
+end
