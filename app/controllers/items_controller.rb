32406f8cb8f7e2bdc89fd876a3d56e4dab4f68a8
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-06-14 02:46:07 +0000

    delete item - no ajax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,15 @@ class ItemsController < ApplicationController
     redirect_to @item.user
   end
   
+  def destroy
+    @item = Item.find(params[:id])
+
+    if !@item.destroy
+       flash[:error] = "Item couldn't be removed. Try again."
+    end
+    redirect_to @item.user
+  end
+  
   private
   def item_params
     params.require(:item).permit(:name, :user_id)
